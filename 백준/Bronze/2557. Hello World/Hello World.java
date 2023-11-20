421833c22c0d5808e6967255135bea0b83ce05dc
23dev-Jin229 <151498026+23dev-Jin229@users.noreply.github.com>
2023-11-20 15:13:55 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,10 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
-        
-        // System.out.println("Hello World!");     - 2번
-        // System.out.printf("Hello World!");      - 3번
-        // System.out.printf("%s","Hello World!"); - 4번
+		//System.out.print("Hello World!");        
+         System.out.println("Hello World!");
+        // System.out.printf("Hello World!");
+        // System.out.printf("%s","Hello World!");
  
 	}
 }
