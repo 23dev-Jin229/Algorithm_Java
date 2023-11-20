1a3b9f42acca6243c8b8115f7f3883ee6347cf22
23dev-Jin229 <151498026+23dev-Jin229@users.noreply.github.com>
2023-11-20 15:05:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
  
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -11,19 +10,24 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
         
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+        
+		/*
+		  range : -1000 ~ 1000
+		  0 은 index[1000] 을 의미
+		*/
+		boolean[] arr = new boolean[2001];
 		
 		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[Integer.parseInt(br.readLine()) + 1000] = true;
 		}
  
-		// 정렬 메소드
-		Arrays.sort(arr);
+		// 정렬 과정이 따로 필요 없음
 		
-		for(int val : arr) {
-			sb.append(val).append('\n');
+		for(int i = 0; i < 2001; i++) {
+			if(arr[i]) {
+				sb.append(i - 1000).append('\n');
+			}
 		}
 		System.out.println(sb);
- 
 	}
 }
